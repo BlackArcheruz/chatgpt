38cca9c53d480345bf48e275ba372fef1c271160
Blackarcheruz <breakingreport4@gmail.com>
2023-03-18 19:09:14 +0500

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,39 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import { useEffect, useState } from "react";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+
+const signIn = () => auth.signInWithPopup(provider);
+const signOut = () => auth.signOut();
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(async (user) => {
+      setUser(user);
+    });
+  }, []);
+
+  return( 
+        <Component  
+        {...pageProps} 
+        user={user} 
+        signIn={signIn} 
+        signOut={signOut}  
+        />
+  )
 }
